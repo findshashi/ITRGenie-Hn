927aa988fcda229168921a5382890e9f4e3ae84a
Shashi Kumar SIngh <47281986+findshashi@users.noreply.github.com>
2026-06-16 16:14:00 +0530

    Fix formatting in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import { appWithTranslation } from "next-i18next";
-import "../styles/globals.css"; // keep your existing import
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-// This single wrapper enables i18n across ALL pages
 export default appWithTranslation(MyApp);
